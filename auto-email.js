79d6e3c5dd3b5fdd3303ed65879bad7e00c51a36
Chris Yuska <chrisyuska@gmail.com>
2013-07-03 15:10:26 -0400

    added support for semi-colon and comma usage in auto completing emails

--- auto-email.js
+++ auto-email.js
@@ -1,58 +1,60 @@
 (function( $ ){
 
-  $.fn.autoEmail = function( domain ) {  
+  $.fn.autoEmail = function( domain ) {
 
     return this.each(function() {
 
-     var $this = $(this); 
-
-      //disable enter key default action
-      $this.keydown(function(e) {
-        var keyCode = e.keyCode ? e.keyCode : e.which ? e.which : e.charCode;
-        if (/^(13)$/.test(""+keyCode)) {
-          e.preventDefault();
-        }
-      });
+     var $this = $(this);
 
       //check for autocomplete after each key
-      $this.keyup(function(e) {
+      $this.keypress(function(e) {
+        e.preventDefault();
         var keyCode = e.keyCode ? e.keyCode : e.which ? e.which : e.charCode;
-        if (/^(13)$/.test(""+keyCode)) {
+        console.log(keyCode);
+        if (/^(13|44|59)$/.test(""+keyCode)) {
           //add comma and space after enter is pressed
-          var emails = $(this).val().trim().split(',');
+          var emails = $(this).val().trim().split(/,\s*|;\s*/);
           var email = emails[emails.length-1];
           if ($this.val().length > 0 && $this.val().trim()[$this.val().trim().length-1] != "," && email.match(/.+@.+\..+/)) {
-            $this.val($this.val()+", ");
+            if (keyCode == 59) {
+              $this.val($this.val()+"; ");
+            } else {
+              $this.val($this.val()+", ");
+            }
           }
+          $this[0].selectionStart = $this.val().length;
+          $this[0].selectionEnd = $this.val().length;
           return;
-        } else if (!/^(8|9|13|16|17|18|46|35|36|37|38|39|40)$/.test(""+keyCode)) {
-          //only look for autocomplete on normal keys (not enter,tab,arrows,etc.)
-          //only look at non-highlighted text, since typer may be faster than script responses
-          var emails = ($(this).val().substring(0,$(this)[0].selectionStart)+$(this).val().substring($(this)[0].selectionEnd, $(this).val().length)).trim().split(',');
+        } else if (!/^(8|9|13|44|59)$/.test(""+keyCode)) {
+          //save selection start for later
+          var selStart = $(this)[0].selectionStart;
+          var selEnd = $(this)[0].selectionEnd;
+          var len = $(this).val().length;
+
+          //replace selection range with typed character
+          $(this).val($(this).val().substring(0,$(this)[0].selectionStart) + String.fromCharCode(keyCode) + $(this).val().substring($(this)[0].selectionEnd, $(this).val().length));
+          len = $(this).val().length - len;
+          //fix selection
+          $this[0].selectionStart = selStart + 1;
+          $this[0].selectionEnd = selEnd + len;
+
+          var emails = $(this).val().trim().split(/,\s*|;\s*/);
           var email = emails[emails.length-1];
           var len = email.length;
           for(i=domain.length;i>=0; i--){
             var re = new RegExp("@"+domain.substr(0,i));
             if (email.substr(email.length-i-1, email.length).match(re) != null) {
+              console.log("replace " + email);
               email = email.replace(re, "@"+domain);
+              console.log("replaced " + email);
               emails[emails.length-1] = email;
-              $this.val(emails.join(','));
+              $this.val(emails.join(', '));
               var start = $this.val().length - email.length + len;
               var end = $this.val().length
 
               //highlight autocompleted text
-              if( $this[0].createTextRange ) {
-                var selRange = $this[0].createTextRange();
-                selRange.collapse(true);
-                selRange.moveStart('character', start);
-                selRange.moveEnd('character', end-start);
-                selRange.select();
-              } else if( $this[0].setSelectionRange ) {
-                $this[0].setSelectionRange(start, end);
-              } else if( $this[0].selectionStart ) {
-                $this[0].selectionStart = start;
-                $this[0].selectionEnd = end;
-              }
+              $this[0].selectionStart = start;
+              $this[0].selectionEnd = end;
               $this.focus();
 
               break;
